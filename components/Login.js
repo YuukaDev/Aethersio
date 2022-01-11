531b1165871e9ad0f344d34217d330125e48ccab
YuukaDev <vule.amv1@gmail.com>
2022-01-11 22:25:56 +0100

    Update Login.js

--- components/Login.js
+++ components/Login.js
@@ -1,17 +1,38 @@
-import React from "react";
-import { auth, provider } from "../auth/firebase";
+import { useState } from "react";
+import {
+  signInWithPopup,
+  GithubAuthProvider,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
+import { auth } from "../auth/firebase";
 
 function Login() {
-  const handleLogin = () => {
-    auth.signInWithPopup(provider);
+  const [user, setUser] = useState({});
+
+  const handleLogin = async () => {
+    try {
+      const user = await signInWithPopup(auth, new GithubAuthProvider());
+      console.log(user);
+      setUser(user);
+    } catch (error) {
+      console.log(error.message);
+    }
   };
-  const handleLogout = () => {
-    auth.signOut();
+
+  const handleLogout = async () => {
+    await signOut(auth);
   };
+
   return (
-    <div>
-      <button onClick={handleLogin}>Sign In With GitHub</button>
-      <button onClick={handleLogout}>Sign Out</button>
+    <div className="App">
+      <h3> GitHub Login </h3>
+
+      <button onClick={handleLogin}>Login With GitHub</button>
+
+      <h4> User Logged In: {user.email}</h4>
+
+      <button onClick={handleLogout}> Sign Out </button>
     </div>
   );
 }

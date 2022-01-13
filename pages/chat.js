e7fe750fb64933f92481d6150d8b3e286632b416
YuukaDev <vule.amv1@gmail.com>
2022-01-13 22:20:48 +0100

    Update chat.js

--- pages/chat.js
+++ pages/chat.js
@@ -1,5 +1,13 @@
-import Login from "../components/Login";
+import ChatContent from "../components/Chat/ChatContent";
+import io from "socket.io-client";
 
 export default function Chat() {
-  return <Login />;
+  const socket = io.connect("http://localhost:3001");
+  const username = "Mario";
+  const room = "Anime Fans"
+  return (
+    <>
+      <ChatContent socket={socket} username={username} room={room} />
+    </>
+  );
 }

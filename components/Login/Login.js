ab5c969b202784bd6f32b2996da2700e2f600954
YuukaDev <vule.amv1@gmail.com>
2022-01-14 23:58:39 +0100

    Update Login.js

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -6,7 +6,7 @@ import {
   signOut,
 } from "firebase/auth";
 import { auth } from "../../auth/firebase";
-import { Button, HStack } from "@chakra-ui/react";
+import { Button, HStack, Input } from "@chakra-ui/react";
 import { FaGithub } from "react-icons/fa";
 import ChatContent from "../Chat/ChatContent";
 
@@ -17,14 +17,22 @@ const socket = io.connect("http://localhost:3001");
 function Login() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [username, setUsername] = useState("");
+  const [data, setData] = useState({});
   const [user, setUser] = useState({});
   onAuthStateChanged(auth, async (currentUser) => {
     setUser(currentUser);
-    const url = await fetch(`https://api.github.com/user/YuukaDev`);
-    const random = await url.json();
-    console.log(random);
     setUsername(currentUser?.email);
   });
+
+  const getUsersData = async (query) => {
+    try {
+      const url = await fetch(`https://api.github.com/user/${query}`);
+      const random = await url.json();
+      console.log(random);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const handleLogin = async () => {
     try {
@@ -54,7 +62,7 @@ function Login() {
 
   return (
     <>
-      {!user ? (
+      {user ? (
         <HStack
           flexDirection="column"
           height="100vh"
@@ -90,6 +98,12 @@ function Login() {
           >
             Logout
           </Button>
+          <Input
+            onChange={(e) => {
+              e.preventDefault();
+              setData(e.target.value);
+            }}
+          />
         </HStack>
       ) : (
         <Main username={user?.additionalUserInfo} />

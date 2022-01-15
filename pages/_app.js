b4f32dd314151079f750ba94f9d5ad99bf746f2a
YuukaDev <vule.amv1@gmail.com>
2022-01-15 23:26:07 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
 import customTheme from "../styles/theme";
+import { ContextProvider } from "../context";
 import "../styles/style.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider resetCSS theme={customTheme}>
+    <ContextProvider>
+      <ChakraProvider resetCSS theme={customTheme}>
         <Component {...pageProps} />
-    </ChakraProvider>
+      </ChakraProvider>
+    </ContextProvider>
   );
 }
 

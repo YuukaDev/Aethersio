4236c59786f4ad2b1060a22273baf01bcc8aeff0
YuukaDev <vule.amv1@gmail.com>
2022-01-16 00:23:14 +0100

    Update Layout.js

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,15 +1,26 @@
-import React from "react";
+import { useContext } from "react";
 import { Divider, VStack } from "@chakra-ui/react";
 
 import Room from "../Rooms/Room";
 import UserProfile from "../UserProfile/UserProfile";
+import { Context } from "../../context";
 
-function Layout({ username, imageSrc }) {
+function Layout() {
+  const {
+    username,
+    setUsername,
+    secret,
+    setSecret,
+    user,
+    setUser,
+    base,
+    setBase,
+  } = useContext(Context);
   return (
     <VStack height="100vh" bg="black" width="20%" overflow="hidden">
       <Room />
       <Divider />
-      <UserProfile username={username} imageSrc={imageSrc} />
+      <UserProfile username={base.login} imageSrc={base.avatar_url} />
     </VStack>
   );
 }

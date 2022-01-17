ea43fd664eef5613d9841e1ca36470b17305c40d
YuukaDev <vule.amv1@gmail.com>
2022-01-17 20:54:43 +0100

    Update index.js

--- context/index.js
+++ context/index.js
@@ -9,36 +9,12 @@ export const ContextProvider = (props) => {
   const [base, setBase] = useState({});
   const [loggedIn, setLoggedIn] = useState(false);
 
-  const handleLogin = async () => {
-    try {
-      const { currentUser } = await signInWithPopup(auth, new GithubAuthProvider());
-      const url = await fetch(
-        `https://api.github.com/users/${user._tokenResponse.screenName}`
-      );
-      const random = await url.json();
-      setBase(random);
-      setUser(currentUser);
-      setLoggedIn(true);
-
-      console.log(random);
-      console.log(user);
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-  const handleLogout = async () => {
-    await signOut(auth);
-    console.log(`User is signed out ${user?.email}`);
-  };
-
   const value = {
-    user,
-    setUser,
     base,
     setBase,
-    handleLogin,
-    handleLogout,
     loggedIn,
+    setUser,
+    user
   };
 
   return <Context.Provider value={value}>{props.children}</Context.Provider>;

00850ac96722ed8d45a2bdf781422286732b2031
YuukaDev <vule.amv1@gmail.com>
2022-01-17 19:36:15 +0100

    Update index.js

--- context/index.js
+++ context/index.js
@@ -1,19 +1,38 @@
 import React, { useState, createContext } from "react";
+import { signInWithPopup, GithubAuthProvider } from "firebase/auth";
 
 export const Context = createContext();
 
 export const ContextProvider = (props) => {
-  const [secret, setSecret] = useState("");
   const [user, setUser] = useState(null);
   const [base, setBase] = useState({});
 
+  const handleLogin = async () => {
+    try {
+      const user = await signInWithPopup(auth, new GithubAuthProvider());
+      setUser(user);
+      console.log(user);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const handleLogout = async () => {
+    if (user != null) {
+      console.log("User is not logged in");
+    } else {
+      await signOut(auth);
+      console.log(`User is signed out ${user?.email}`);
+    }
+  };
+
   const value = {
-    secret,
-    setSecret,
     user,
     setUser,
     base,
     setBase,
+    handleLogin,
+    handleLogout,
   };
 
   return <Context.Provider value={value}>{props.children}</Context.Provider>;

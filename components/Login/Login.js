17f8263edcd08192cef07251a467c9b50ec8f3cd
YuukaDev <vule.amv1@gmail.com>
2022-01-18 15:09:30 +0100

    Update Login.js

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -1,10 +1,4 @@
-import { useContext, useEffect } from "react";
-import {
-  signInWithPopup,
-  GithubAuthProvider,
-  onAuthStateChanged,
-  signOut,
-} from "firebase/auth";
+import { useContext } from "react";
 import { auth } from "../../auth/firebase";
 import { Button, HStack } from "@chakra-ui/react";
 import { FaGithub } from "react-icons/fa";
@@ -13,35 +7,13 @@ import { useRouter } from "next/router";
 
 import { useAuthState } from "react-firebase-hooks/auth";
 
-import io from "socket.io-client";
-import Main from "../Main/Main";
-const socket = io.connect("http://localhost:3001");
+//import io from "socket.io-client";
+//const socket = io.connect("http://localhost:3001");
 
 function Login() {
   const router = useRouter();
   const [user, loading, error] = useAuthState(auth);
-  const { userCred, setUserCred } = useContext(Context);
-
-  const handleLogin = async () => {
-    try {
-      const userData = await signInWithPopup(auth, new GithubAuthProvider());
-      console.log(user._tokenResponse.screenName);
-
-      setUserCred(userData);
-      console.log(user);
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-
-  const handleLogout = async () => {
-    if (user) {
-      await signOut(auth);
-      console.log(`User is signed out ${user?.email}`);
-    } else {
-      console.log("User is not logged in");
-    }
-  };
+  const { handleLogin, handleLogout } = useContext(Context);
 
   if (loading) {
     return (

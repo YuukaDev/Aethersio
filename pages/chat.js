55d660bf417c193d55576ed8057bc12584caa17d
YuukaDev <vule.amv1@gmail.com>
2022-01-18 14:35:54 +0100

    Update chat.js

--- pages/chat.js
+++ pages/chat.js
@@ -1,10 +1,24 @@
 //import ChatContent from "../components/Chat/ChatContent";
+
+import { useContext } from "react";
+
 import io from "socket.io-client";
 import Main from "../components/Main/Main";
+import Login from "../components/Login/Login";
+import { Context } from "../context";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../auth/firebase";
+
 export default function Chat() {
+  const { userCred } = useContext(Context);
+  const [currentUser] = useAuthState(auth);
   return (
     <>
-      <h1>Hello</h1>
+      {currentUser ? (
+        <Main username={userCred?.email} imageSrc={currentUser?.photoURL} />
+      ) : (
+        <Login />
+      )}
     </>
   );
 }

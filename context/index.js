611fe9024734d449a309bd72af734f2393e339a7
YuukaDev <vule.amv1@gmail.com>
2022-01-18 14:35:53 +0100

    Update index.js

--- context/index.js
+++ context/index.js
@@ -5,19 +5,13 @@ import { auth } from "../auth/firebase";
 export const Context = createContext();
 
 export const ContextProvider = (props) => {
-  const [user, setUser] = useState(null);
-  const [anotherUser, setAnotherUser] = useState({});
+  const [userCred, setUserCred] = useState(null);
+  //const [anotherUser, setAnotherUser] = useState({});
   const [base, setBase] = useState({});
-  const [loggedIn, setLoggedIn] = useState(false);
 
   const value = {
-    base,
-    setBase,
-    loggedIn,
-    setUser,
-    user,
-    anotherUser,
-    setAnotherUser,
+    userCred,
+    setUserCred,
   };
 
   return <Context.Provider value={value}>{props.children}</Context.Provider>;

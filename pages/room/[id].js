6ea29e158ecf2017b3a60560d611913d1525b83f
YuukaDev <vule.amv1@gmail.com>
2022-01-19 19:52:14 +0100

    Update [id].js

--- pages/room/[id].js
+++ pages/room/[id].js
@@ -1,7 +1,34 @@
-import React from "react";
+import { useContext } from "react";
+import { Context } from "../../context";
+
+import ChatContent from "../../components/Chat/ChatContent";
+import io from "socket.io-client";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../auth/firebase";
+
+import { Container, Box } from "@chakra-ui/react";
 
 function RoomPage() {
-  return <div>Hello</div>;
+  const [user] = useAuthState(auth);
+  const socket = io.connect("http://localhost:3001");
+  const { room } = useContext(Context);
+  return (
+    <Container>
+      <Box
+        mt="3em"
+        width="100%"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <ChatContent
+          socket={socket}
+          username={user?.reloadUserInfo.screenName}
+          room={room}
+        />
+      </Box>
+    </Container>
+  );
 }
 
 export default RoomPage;

836395b50e0ccfff0b94a5dc4293446f9a3f0db7
YuukaDev <vule.amv1@gmail.com>
2022-01-19 19:36:12 +0100

    Update UserProfile.js

--- components/UserProfile/UserProfile.js
+++ components/UserProfile/UserProfile.js
@@ -3,12 +3,17 @@ import { Container, Box, Flex, Heading, Avatar } from "@chakra-ui/react";
 
 function UserProfile({ username, imageSrc }) {
   return (
-    <Container w="100%" h="100%" bg="red">
-      <Box>
-        <Flex mt="1em" gap="10px" justifyContent="center" alignItems="center">
-          <Heading size="lg">{username}</Heading>
-          <Avatar src={imageSrc} />
-        </Flex>
+    <Container w="100%" bg="red">
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        gap="10px"
+        mt="1em"
+        mb="1em"
+      >
+        <Heading size="lg">{username}</Heading>
+        <Avatar src={imageSrc} />
       </Box>
     </Container>
   );

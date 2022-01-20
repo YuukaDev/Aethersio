1500209d1195d767b1adeae157da703b04f72767
YuukaDev <vule.amv1@gmail.com>
2022-01-20 15:48:23 +0100

    Update room.js

--- pages/room.js
+++ pages/room.js
@@ -1,12 +1,9 @@
-import { useEffect } from "react";
-import { useRouter } from "next/router";
 import Main from "../components/Main/Main";
 import Login from "../components/Login/Login";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../auth/firebase";
 
 export default function Chat() {
-  const router = useRouter();
   const [user] = useAuthState(auth);
   const sendData = async () => {
     console.log(currentUser);

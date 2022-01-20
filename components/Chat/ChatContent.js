a9eff1152f240b91d4c3ae06ce72361ed93dfe65
YuukaDev <vule.amv1@gmail.com>
2022-01-20 15:48:32 +0100

    Update ChatContent.js

--- components/Chat/ChatContent.js
+++ components/Chat/ChatContent.js
@@ -12,7 +12,7 @@ import {
 import moment from "moment";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../auth/firebase";
-import Login from "../Login/Login";
+import { ChatIcon } from "@chakra-ui/icons";
 
 function ChatContent({ socket, username, room }) {
   const [user] = useAuthState(auth);
@@ -41,51 +41,61 @@ function ChatContent({ socket, username, room }) {
   }, [socket]);
 
   return (
-    <Box className="chat-window">
-      <div className="chat-header">
-        <p>Live Chat</p>
-      </div>
-      <div className="chat-body">
+    <>
+      <Box>
         {messageList.map((messageContent, index) => {
           return (
-            <Box>
-              <Flex gap="10px">
+            <Box mt="25px" key={index}>
+              <Box
+                gap="10px"
+                display="flex"
+                justifyContent="center"
+                alignContent="center"
+              >
                 <Avatar src={user?.photoURL} />
-                <Heading>{messageContent.author}</Heading>
-              </Flex>
-              <Text fontSize="1.5em">{messageContent.message}</Text>
-              <Text fontSize="md" as="samp">
-                {messageContent.time}
-              </Text>
+                <Heading fontSize="1.5em">{messageContent.author}</Heading>
+                <Text fontSize="1.3em"> | </Text>
+                <Text fontSize="1.3em" as="samp">
+                  {messageContent.time}
+                </Text>
+              </Box>
+              <Box>
+                <Flex justifyContent="left" alignItems="center" ml="60px">
+                  <Text textAlign="left" mt="-20px" fontSize="1.5em">
+                    {messageContent.message}
+                  </Text>
+                </Flex>
+              </Box>
             </Box>
           );
         })}
-      </div>
-      <Box className="chat-footer">
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            const value = e.target.elements.inputValue.value;
-            setCurrentMessage(value);
-          }}
-        >
-          <Flex gap="10px">
-            <Input
-              id="inputValue"
-              type="text"
-              value={currentMessage}
-              placeholder="Hey..."
-              onChange={(e) => {
-                setCurrentMessage(e.target.value);
-              }}
-            />
-            <Button type="submit" onClick={sendMessage}>
-              &#9658;
-            </Button>
-          </Flex>
-        </form>
+        <br />
+        <Box width="100%" margin="0 auto">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              const value = e.target.elements.inputValue.value;
+              setCurrentMessage(value);
+            }}
+          >
+            <Flex gap="10px">
+              <Input
+                id="inputValue"
+                type="text"
+                value={currentMessage}
+                placeholder="Hey..."
+                onChange={(e) => {
+                  setCurrentMessage(e.target.value);
+                }}
+              />
+              <Button type="submit" onClick={sendMessage}>
+                <ChatIcon />
+              </Button>
+            </Flex>
+          </form>
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 }
 

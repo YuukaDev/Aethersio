1df7dc0e586395980fc9ed7869766c9f86a725f5
YuukaDev <vule.amv1@gmail.com>
2022-01-23 23:50:41 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import Login from "../components/Login/Login";
 import Main from "../components/Main/Main";
 import { useAuthState } from "react-firebase-hooks/auth";

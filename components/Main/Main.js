c3b270f2171dd24a8f0478e6561c65b0aeab5aad
YuukaDev <vule.amv1@gmail.com>
2022-02-25 23:06:14 +0100

    Update Main.js

--- components/Main/Main.js
+++ components/Main/Main.js
@@ -1,11 +1,14 @@
+import { Box } from "@chakra-ui/react";
 import React from "react";
 import Layout from "../Layout/Layout";
+import Room from "../Rooms/Room";
+import SideBar from "../SideBar/SideBar";
 
 function Main() {
   return (
-    <div>
-      <Layout />
-    </div>
+    <Box>
+      <SideBar />
+    </Box>
   );
 }
 

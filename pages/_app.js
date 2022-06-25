fb8b421c259e17b6639ced659341051cbe6e2b33
YuukaDev <vule.amv1@gmail.com>
2022-06-25 23:48:15 +0200

    Deleted Context Provider ✨

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,11 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import customTheme from "../theme/theme";
-import { ShopProvider } from "../utils/StoreContext";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ShopProvider>
-      <ChakraProvider resetCSS theme={customTheme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </ShopProvider>
+    <ChakraProvider resetCSS theme={customTheme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
   );
 }
 
